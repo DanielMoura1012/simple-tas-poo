c1ba555a2a61195558b2015cb808bfd901d6903c
DanielMoura1012 <danielrbmoura1012@gmail.com>
2024-08-30 22:30:59 +0000

    feat: criação de codigo

---
+++ aula.java
@@ -0,0 +1,21 @@
+//classe principal
+
+public class Tarefa{
+    private String titulo;
+    private String descricao;
+    private String prazo;
+    private int propiedade;
+}
+
+// metodos get e set
+
+public String getTitulo() {
+    return this.titulo;
+}
+
+
+
+public void setTitulo(String titulo) {
+    this.titulo=titulo;
+}
+
